d9a587f13406ae959f00ad2c755306bee00f42f6
San4oPanso <san4opansosp@gmail.com>
2016-04-07 12:32:21 +0300

    Changed appState to not return state if prop defined

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,37 +3,43 @@ import {HmrState} from 'angular2-hmr';
 
 @Injectable()
 export class AppState {
-  // HmrState uis used by HMR to track the any state during reloading
-  @HmrState() _state = {};
+    // HmrState uis used by HMR to track the any state during reloading
+    @HmrState() _state = {};
 
-  constructor() {
+    constructor() {
 
-  }
+    }
 
-  // already return a clone of the current state
-  get state() {
-    return this._state = this._clone(this._state);
-  }
-  // never allow mutation
-  set state(value) {
-    throw new Error('do not mutate the `.state` directly');
-  }
-
-
-  get(prop?: any) {
-    // use our state getter for the clone
-    const state = this.state;
-    return state[prop] || state;
-  }
-
-  set(prop: string, value: any) {
-    // internally mutate our state
-    return this._state[prop] = value;
-  }
+    // already return a clone of the current state
+    get state() {
+        return this._state = this._clone(this._state);
+    }
+    // never allow mutation
+    set state(value) {
+        throw new Error('do not mutate the `.state` directly');
+    }
 
 
-  _clone(object) {
-    // simple object clone
-    return JSON.parse(JSON.stringify( object ));
-  }
+    get(prop?: any) {
+        // use our state getter for the clone
+        const state = this.state;
+        if (prop === undefined) {
+            return state;
+        }
+
+        return state[prop];
+    }
+
+    set(prop: string, value: any) {
+        // internally mutate our state
+        return this._state[prop] = value;
+    }
+
+
+    _clone(object) {
+        // simple object clone
+        return JSON.parse(JSON.stringify(object));
+    }
+
+
 }
